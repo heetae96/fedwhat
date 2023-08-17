8b96f45f685ffb8f84fa75e0e3227f12c279d558
2sookwang <2sookwang@gmail.com>
2023-08-17 15:28:01 +0900

    Trying use GN or LN instead of BN.

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -10,25 +10,34 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
+'''
+Trying use group normalize and layer normalize
+'''
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1, size=32):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
+        #self.bn1 = nn.BatchNorm2d(planes)
+        self.bn1 = nn.GroupNorm(2, planes)
+        #self.bn1 = nn.LayerNorm([planes, size, size])
+        
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
                                stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
+        #self.bn2 = nn.BatchNorm2d(planes)
+        self.bn2 = nn.GroupNorm(2, planes)
+        #self.bn2 = nn.LayerNorm( [planes, size, size])
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes,
                           kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
+                #nn.BatchNorm2d(self.expansion*planes)
+                nn.GroupNorm(2, self.expansion*planes)
+                #nn.LayerNorm( [self.expansion*planes, size, size])
             )
 
     def forward(self, x):
@@ -45,10 +54,14 @@ class Bottleneck(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
+        #self.bn1 = nn.BatchNorm2d(planes)
+        self.bn1 = nn.GroupNorm(2, planes)
+        #self.bn1 = nn.LayerNorm(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
                                stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
+        #self.bn2 = nn.BatchNorm2d(planes)
+        self.bn2 = nn.GroupNorm(2, planes)
+        #self.bn2 = nn.LayerNorm( planes)
         self.conv3 = nn.Conv2d(planes, self.expansion *
                                planes, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(self.expansion*planes)
@@ -58,7 +71,9 @@ class Bottleneck(nn.Module):
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes,
                           kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
+                #nn.BatchNorm2d(self.expansion*planes)
+                nn.GroupNorm(2, self.expansion*planes)
+                #nn.LayerNorm( self.expansion*planes)
             )
 
     def forward(self, x):
@@ -77,18 +92,20 @@ class ResNet(nn.Module):
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        #self.bn1 = nn.BatchNorm2d(64)
+        self.bn1 = nn.GroupNorm(2, 64)
+        #self.bn1 = nn.LayerNorm( [64, 32, 32])
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1, size=32)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2, size=16)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2, size=8)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2, size=4)
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
-    def _make_layer(self, block, planes, num_blocks, stride):
+    def _make_layer(self, block, planes, num_blocks, stride, size=32):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
+            layers.append(block(self.in_planes, planes, stride, size))
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
@@ -98,14 +115,15 @@ class ResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
+        #out = F.avg_pool2d(out, 4)
+        out = F.adaptive_avg_pool2d(out, 1)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
 
 
 def ResNet18():
-    return ResNet(BasicBlock, [2, 2, 2, 2], 62)
+    return ResNet(BasicBlock, [2, 2, 2, 2])
 
 
 def ResNet34():
