52908139be55b5d416ab798187f27ecb8e144b9b
Scloin <scloin9763@gmail.com>
2023-08-17 18:50:50 +0900

    Deprecated merge code(same with fedavg)

--- src/learning/_merge_param.py
+++ src/learning/_merge_param.py
@@ -3,43 +3,6 @@ import torch
 from abc import abstractmethod
 import copy
 
-def merge_param(P : list((int,int,OrderedDict([(str, torch.Tensor),])),), device):
-
-    par = P[0][2]
-    '''
-    for p in P:
-        print(p[1],end=' ')
-    print()
-
-
-    if isinstance(par, OrderedDict):
-        """
-        case when use model.statedict.
-        """
-        for i in range(1,len(P)):
-            for key in par.keys():
-                par[key]=torch.add(par[key],(P[i][2][key]).to(torch.device(device)))
-        for key in par.keys():
-            par[key] = par[key]/len(P)
-
-    elif isinstance(par, list):
-        """
-        case when use model.param etc.
-        """ 
-        with torch.no_grad():
-            for i in range(1,len(P)):
-                for j in range(len(par)):
-                    par[j].add_(P[i][2][j].to(torch.device(device)))
-            for j in range(len(par)):
-                par[j] = par[j]/len(P)
-    else:
-        raise TypeError("parameter type is not supported")
-    '''
-    
-    optim = FedYogi()
-    aggregated = optim.do(par=par,P=P,dev=device)
-    
-    return aggregated
 
 class FedAvg_modif:
     '''
